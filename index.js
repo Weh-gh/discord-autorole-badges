584f953a0329a2bb35ac453d5ce8415f1017f1d3
Mark <87272164+Weh-Infinity@users.noreply.github.com>
2022-01-22 01:14:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ client.on("guildMemberAdd", async (member) => {
 })
 
 client.on("ready", () => {
-    console.log("Discord login successfully")
+    console.log("Discord login successful!")
 })
 
 client.login(Discord_Bot_Token)
